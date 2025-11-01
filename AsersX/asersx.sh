533ca4b251a6b4de12570dc4da2159485fa74cb9
TermuxnTR <aseldurubaysal@gmail.com>
2025-11-01 14:22:22 +0300

    Update asersx.sh

--- AsersX/asersx.sh
+++ AsersX/asersx.sh
@@ -4,7 +4,7 @@
 DISTRO_NAME="AsersX"
 DISTRO_COMMENT="AsersX Rootfs OS"
 
-TARBALL_URL['aarch64']="https://download1529.mediafire.com/cu838oxmctbgIti-PNSf_2uQsHPoT3JDe_FQlyipmskiCwxo4-GEDb68h0sCvNmIfZV12dWzPq1cpHSktIOs5PwgHRZ5KONWYhY45WQfjKBg_jdWwgajR7ql0ps1_Y4M7_gbY4O7JiGTwc45rdCBgyshjBDq5FWaKbVBLBub1065WQ/j6ohh1p6wegy7sr/asersxARM64.tar.xz"
+TARBALL_URL['aarch64']="https://github.com/TermuxnTR/AsersX-for-Termux/releases/download/v1.0.0/asersxARM64.tar.xz"
 TARBALL_SHA256['aarch64']="0d3c7219f4fc79df44e9438827a8805ecb452a6a9899975a83963a09eba2bca3"
 
 TARBALL_STRIP_OPT=0
