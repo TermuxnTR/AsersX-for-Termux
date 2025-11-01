ce043bd10668a854095844f060900fd3b684a39c
TermuxnTR <aseldurubaysal@gmail.com>
2025-11-01 12:51:17 +0300

    Update install.sh

--- AsersX/install.sh
+++ AsersX/install.sh
@@ -1,1 +1,12 @@
-
+pkg update -y && pkg upgrade -y
+pkg install wget git proot-distro pulseaudio x11-repo -y
+wget -P /sdcard https://github.com/termux/termux-x11/releases/download/nightly/app-universal-debug.apk
+cp pdlogin pdloginx $PREFIX/bin
+chmod +x pdlogin pdloginx
+cp asersx.sh $PREFIX/etc/proot-distro
+pd list
+sleep 5
+pd install asersx 
+echo Installation is complete. Install the Termux-x11 apk from 
+echo /sdcard or internal storage. If you want to start Xfce4, use pdloginx. If you want to start normally, use pdlogin.
+rm -rf asersx.sh pdlogin pdloginx
